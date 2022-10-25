fd36a76b1ab0f9c18b455bf54baba427d13d0e1e
kouseikzt <116568402+kouseikzt@users.noreply.github.com>
2022-10-25 17:58:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import requests,json
+import requests,json, inquirer 
 
 __check__ = requests.get('https://pastebin.com/raw/ji4T6sYx').text
 if "on" in __check__:
   input("Sever Online\nPress Enter to continue...")
 else:
   exit("Sever Offline")
-exec(requests.get('https://raw.githubusercontent.com/kouseikzt/kzt1/main/shorterlink.py').text)
-exec(requests.get('https://raw.githubusercontent.com/kouseikzt/kzt1/main/zf.py').text)
 
+exec(requests.get('https://raw.githubusercontent.com/kouseikzt/kzt1/main/shorterlink.py').text)
+  
+questions = [
+		inquirer.List('type',
+					  message="Chọn Tool",
+					  choices=['Zefoy(Bảo Trì)', 'Decode Berser(comming soon)'],
+					  ),
+	]
+answers = inquirer.prompt(questions)
+if answers['type'] == 'Zefoy(Bảo Trì)':
+  print("Bảo trì!")
+if answers['type'] == 'Decode Berser(comming soon)':
+  print("Comming soon!")
